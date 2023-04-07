5606f0d5de7551a15869c0adc6a6a78c9cf630a7
alexander <alexgalimov2003@mail.ru>
2023-04-07 20:07:39 +0300

    commit_fix

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from PIL import Image, ImageEnhance
 from flask import Flask, render_template, request, send_from_directory
 
 APP_ROUTE = os.path.dirname(os.path.abspath(__file__))
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'HEIC'}
 
 app = Flask(__name__)
 
@@ -27,19 +26,16 @@ def send_image(filename):
 
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
-
     target = os.path.join(APP_ROUTE, 'static/images')
-    # target = os.path.join(APP_ROOT, 'static/')
-    print(target)
-    if not os.path.isdir(target):
-        os.mkdir(target)
-    else:
-        print("Couldn't create upload directory: {}".format(target))
     print(request.files.getlist("file"))
     for file in request.files.getlist("file"):
-        print(file)
+        if file.filename == "":
+            filename = "er.png"
+            return render_template("complete.html", image_name=filename)
+
         print("{} is the file name".format(file.filename))
         filename = file.filename
+        print("fi" + filename)
         destination = "/".join([target, filename])
         print("Accept incoming file:", filename)
         print("Save it to:", destination)
@@ -49,6 +45,22 @@ def upload():
             b = request.form['brightness']
             brightness = float(b)
             change_brightness(destination, brightness)
+
+            a = request.form['rotate']
+            angle = int(a)
+            rotate_image(destination, angle)
+
+            left = request.form['left']
+            upper = request.form['upper']
+            right = request.form['right']
+            lower = request.form['lower']
+            left_parsed = int(left)
+            upper_parsed = int(upper)
+            right_parsed = int(right)
+            lower_parsed = int(lower)
+            crop_image(destination, left_parsed, upper_parsed, right_parsed, lower_parsed)
+
+
         except TypeError:
             print("error")
         except ValueError:
@@ -64,6 +76,18 @@ def change_brightness(filename, koef):
     img = enhancer.enhance(koef)
 
     img.save(filename)
+
+
+def rotate_image(filename, angle):
+    img = Image.open(filename)
+    rotated_img = img.rotate(angle)
+    rotated_img.save(filename)
+
+
+def crop_image(filename, left, upper, right, lower):
+    img = Image.open(filename)
+    im_crop = img.crop((left, upper, right, lower))
+    im_crop.save(filename)
 
 
 if __name__ == "__main__":
